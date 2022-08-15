9566756c54f5151f1530e8c99b2dc6ce13195291
MRPhillip007 <filipplyamov@gmail.com>
2022-08-15 15:36:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+class Homework
+{
+    static void Main()
+    {
+        Console.WriteLine(SortInputStringByAlphabet("hello")); 
+    }
+
+    static string SortInputStringByAlphabet(string str) // hello - > ehllo
+    {
+        char[] chars = str.ToCharArray();
+
+        foreach (char c in chars)
+            Console.Write(c + " ");
+        Array.Sort(chars);
+
+        return new string(chars);
+    }
+
+    enum CharactersAlphabetPosition
+    {
+        samePosition = 0,
+        Before = -13,
+        After = 25,
+    }
+}
